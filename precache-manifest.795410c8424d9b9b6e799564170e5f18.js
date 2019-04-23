e7416e6a5b8d7eee433bcf38402b20fd29f577f0
Kosuke Abe <kousuke1201abe@gmail.com>
2019-04-23 22:01:53 +0900

    Updates

---
+++ precache-manifest.795410c8424d9b9b6e799564170e5f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3934237d9fb215f92cc8",
+    "url": "/portfolio/static/css/main.0ad2f446.chunk.css"
+  },
+  {
+    "revision": "3934237d9fb215f92cc8",
+    "url": "/portfolio/static/js/main.5ec24814.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a6f143a09e9bf8b86ff1b262816980e3",
+    "url": "/portfolio/index.html"
+  }
+];
